7c8fba23932793068dac53f9e687c990ce8ec087
MohamedMagdyMohamed <mm.medany96@gmail.com>
2022-08-02 08:00:19 +0200

    Add body-parser

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 
+// Project Dependencies
+const bodyParser = require("body-parser");
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));

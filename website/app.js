5885e600b8316d8d507be93d9e2caccc2355047f
MohamedMagdyMohamed <mm.medany96@gmail.com>
2022-08-02 08:09:43 +0200

    Add API Credentials

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = '48687b1f0e51231060260cbd3c10dfc8&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();

1de60f451f1c8e9eee11d6f3dfdc780a72c60ab7
MohamedMagdyMohamed <mm.medany96@gmail.com>
2022-08-02 08:00:34 +0200

    Add cors

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 
-// Project Dependencies
-const bodyParser = require("body-parser");
-
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
